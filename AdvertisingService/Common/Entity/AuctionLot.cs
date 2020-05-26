d9f6a726d278bd2f7b62740a2d01ac56a7354434
ZhenyaTrif <nomand144@gmail.com>
2020-05-26 12:41:20 +0300

    Add AuctionLot class

---
+++ AdvertisingService/Common/Entity/AuctionLot.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Entity
+{
+    public class AuctionLot
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string LotName { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Required]
+        public string StartPrice { get; set; }
+
+        [Required]
+        public int LotCategoryId { get; set; }
+
+        public string CreaterId { get; set; }
+
+        public bool Checked { get; set; }
+
+        public bool Opened { get; set; }
+
+        public string WinnerId { get; set; }
+    }
+}
